7eb7676ccacdadc87d619d73a3e15eb251a456f8
Mkxyz24 <mohith.kmk@gmail.com>
2022-06-01 05:03:22 +0530

    reduced and specific calling

--- utils/make_call.py
+++ utils/make_call.py
@@ -3,18 +3,7 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
-def check_intersection(user,courses):
-    #desired specific list
-    desired_1 = ['92030', '70517', '81285', '81289', '90104', '97669',
-                     '96730', '76770', '98070', '75623', '83713', '92173', '96290', '78322',
-                     '86207', '96593', '86208', '77802', '83405', '96739', '78302',
-                     '84856', '86209', '96727', '89746', '87271']
-    
-    desired_2 = ['97669', '96730', '76770', '96290']
-    desired_3 = ['96727','84856','96730','86208','76055']
-    desired_4 = ['78302', '98225','96292']
-
-    desired = [desired_1,desired_2, desired_3, desired_4]
+def check_intersection(user,desired,courses):
     set_available = set([d['id'] for d in courses])  
     set_desired = set(desired[user])
     open = set_available.intersection(set_desired)
@@ -29,10 +18,23 @@ def make_call(users, courses):
     account_sid = os.getenv('TWILIO_ACCOUNT_SID')
     auth_token = os.getenv('TWILIO_AUTH_TOKEN')
     client = Client(account_sid, auth_token)
+
+    #desired specific list
+    desired_1 = ['92030', '70517', '81285', '81289', '90104', '97669',
+                     '96730', '76770', '98070', '75623', '83713', '92173', '96290', '78322',
+                     '86207', '96593', '86208', '77802', '83405', '96739', '78302',
+                     '84856', '86209', '96727', '89746', '87271']
+    
+    desired_2 = ['97669', '96730', '76770', '96290'] #dds,dps,cc,dvp
+    desired_3 = ['96727','84856','96730','86208'] #ai,nlp,dps
+    desired_4 = ['78302', '98225','96292']          #sml,planning/ai
+
+    desired = [desired_1,desired_2, desired_3, desired_4]
+
     for i,user in enumerate(users):
         num = os.getenv(user)
         #print(num)
-        open = check_intersection(i,courses)
+        open = check_intersection(i,desired,courses)
         #ab all day calling
         #if user == 'NUM_1' or user == 'NUM_4' or user == 'NUM_2':
         if(len(open) > 0):
