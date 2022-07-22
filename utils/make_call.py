14011aad8957d1f4a13886fe6ce90d10d93bae9d
Mkxyz24 <mohith.kmk@gmail.com>
2022-07-22 20:33:59 +0530

    dps

--- utils/make_call.py
+++ utils/make_call.py
@@ -22,7 +22,7 @@ def make_call(users, courses):
     #desired specific list
     desired_1 = ['76770','96730','96727','86208','78302','98225','86207']
     desired_2 = ['96730', '76055'] 
-    desired_3 = ['96730','76770'] 
+    desired_3 = ['96730'] 
     desired_4 = ['78302', '98225','96292']          
 
     desired = [desired_1,desired_2, desired_3, desired_4]
