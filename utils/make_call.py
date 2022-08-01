d39af6e5b057ccbd4db9f1dffc1c69d61e916caf
Mkxyz24 <mohith.kmk@gmail.com>
2022-08-02 00:10:18 +0530

    yoyoyo

--- utils/make_call.py
+++ utils/make_call.py
@@ -21,7 +21,7 @@ def make_call(users, courses):
 
     #desired specific list
     desired_1 = ['76770','96730','96727','86208','78302','98225','86207']
-    desired_2 = ['96730', '83470', '76770', '85298', '96593', '86208', '77802', '83405', '97807'] 
+    desired_2 = ['97807'] 
     desired_3 = ['96730'] 
 
     desired_4 = ['78302', '98225','96292']          
@@ -36,7 +36,7 @@ def make_call(users, courses):
         #ab all day calling
 
         if(len(open) > 0):
-            if user == 'NUM_5' :
+            if user == 'NUM_3' or user == 'NUM_2':
                 call = client.calls.create(
                     twiml='<Response><Say>Hello ' + user + ', your desired courses are open!</Say></Response>',
                     to = num,
